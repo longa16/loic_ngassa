915a423d0e1227939f47134da82491b72e586af4
longa16 <yannngassa35@gmail.com>
2025-08-01 11:51:05 +0200

    feat: other file

--- script/other.js
+++ script/other.js
@@ -21,24 +21,21 @@ document.addEventListener('DOMContentLoaded', function() {
     const carouselData = {
         'design-content': [
             { img: 'assets/service-civique1.jpg', caption: 'Atelier de formation' },
-            { img: 'assets/service-civique2.png', caption: 'Newsletter' },
-            { img: 'assets/service-civique2.png', caption: 'Newsletter' },
+            { img: 'https://lejour.cm/wp-content/uploads/2024/05/IMG-20240531-WA0002-1280x853.jpg', caption: 'Stand MBOA Paris'},
+            { img: 'https://medias.objectifgard.com/api/v1/images/view/683014953fdc091d930edbe8/article/image.jpg', caption: 'mentorat afev' },
             { img: 'assets/service-civique2.png', caption: 'Newsletter' },
             { img: 'assets/Mboa-Paris.webp', caption: 'MBOA Paris 2024' },
         ],
         'engineering-content': [
+            { img: 'https://media.licdn.com/dms/image/v2/D4E22AQGAsQKzPb2cFQ/feedshare-shrink_2048_1536/B4EZdaGZpzH0Ao-/0/1749563319245?e=1756944000&v=beta&t=0NUc8JbT2IjkK657Q_RcWB3E7rPgfwa955pNGfzsSPs', caption: 'ICMAIE 2025"' },
             { img: 'assets/conference1.jpg', caption: 'VivaTech 2025 "Tesla Optimus"' },
-            { img: 'assets/conference1.jpg', caption: 'VivaTech 2025 "Tesla Optimus"' },
-            { img: 'assets/conference1.jpg', caption: 'VivaTech 2025 "Tesla Optimus"' },
-            { img: 'assets/conference1.jpg', caption: 'VivaTech 2025 "Tesla Optimus"' },
+            { img: 'https://media.licdn.com/dms/image/v2/D4E22AQEXJtLah3OXfQ/feedshare-shrink_2048_1536/B4EZdaGZpuHgAo-/0/1749563319542?e=1756944000&v=beta&t=87pu-JIK_fKFREuisoPserlHhsv5ojqA_F48iobFk-8', caption: 'ICMAIE 2025"' },
+            { img: 'https://media.licdn.com/dms/image/v2/D4E12AQGqqNSP9rLIfw/article-cover_image-shrink_600_2000/B4EZePM7W4G4AU-/0/1750454222025?e=1759363200&v=beta&t=1smpTsSkIKe_pQORJpreh1i2t2zIFoX22JO_iJb6P-c', caption: 'VivaTech 2025' },
             { img: 'assets/conference2.jpg', caption: 'Prototype fusée' }
         ],
         'photography-content': [
-            { img: 'assets/sport1.png', caption: 'Match de basketball' },
-            { img: 'assets/sport1.png', caption: 'Match de basketball' },
-            { img: 'assets/sport1.png', caption: 'Match de basketball' },
-            { img: 'assets/sport1.png', caption: 'Match de basketball' },
-            { img: 'assets/sport2.jpg', caption: 'Course à pied' }
+            { img: 'assets/sport1.png', caption: 'Marathon 2024' },
+            { img: 'https://media.sudouest.fr/14316293/1000x500/capture.jpg?v=1678111233', caption: 'Match de basketball' },
         ],
     };
 
